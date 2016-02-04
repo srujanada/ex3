2fb345f29dba1af7989b1598c53069b43688e9fa
Srujana Danda <ssdanda@gmail.com>
2016-02-04 14:34:04 -0800

    after fixing

--- ex3.js
+++ ex3.js
@@ -6,16 +6,23 @@ $(document).ready(function() {
   
   $('section#blocks li').mouseover(function() {
     //change the #mouseover background color
-    $('section#blocks li').css('background-color','#927B51');
+    $(this).css('background-color','#927B51');
   });
   
   $('section#blocks li').mouseout(function() {
     //change the #mouseout background color to original
-    $('section#blocks li').css('background-color','#330c00');
+    $(this).css('background-color','#330c00');
   });
   
+  //HIDE ALL initially
+  $('#workText').hide();
+  $('#restText').hide();
+  $('#eatText').hide();
+  $('#smileText').hide();
+  
+  
+  //HOVER FUNCTIONS
   $('#work').hover(function() { // first function
-
       //turn on the tool tip
       $('#workText').show();}, 
     function() { // second function
@@ -42,5 +49,42 @@ $(document).ready(function() {
     function() { // second function
       $('#smileText').hide();
     });
+  
+  //CLICK FUNCTIONS
+  $('#work').click(function() { // first function
+  	  console.log("click working");
+      $('#workMain p').css('color','black');
+    /*},
+    function() { // second function
+      $('#workMain p').css('color','#ff6533');
+    return false;  */
+	});
+
+	$('#rest').click(function() { // first function
+  	  console.log("click resting");
+      $('#restMain p').css('color','black');
+    /*},
+    function() { // second function
+      $('#resMain p').css('color','#ff6533');
+    return false;  */
+	});
+	
+	$('#eat').click(function() { // first function
+  	  console.log("click eating");
+      $('#eatMain p').css('color','black');
+    /*},
+    function() { // second function
+      $('#eatMain p').css('color','#ff6533');
+    return false;  */
+	});
+	
+	$('#smile').click(function() { // first function
+  	  console.log("click smiling");
+      $('#smileMain p').css('color','black');
+    /*},
+    function() { // second function
+      $('#smileMain p').css('color','#ff6533');
+    return false;  */
+	});
   
 });
